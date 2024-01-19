e2b533e33b345e19ef44475f8a86754575dd81cb
PatricioTristante <6808469@alu.murciaeduca.es>
2024-01-19 12:21:58 +0100

    Esqueleto

---
+++ src/componentes/cabecera/cabecera.jsx
@@ -0,0 +1,12 @@
+import './cabecera.css'
+
+function Cabecera() {
+  
+  return (
+    <header className='bg-primary text-white text-center marginAbajo'>
+        <h1>PATATA 2</h1>
+    </header>
+  )
+}
+
+export default Cabecera
