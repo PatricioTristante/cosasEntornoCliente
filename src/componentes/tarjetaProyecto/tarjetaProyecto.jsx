7f266621e838938473751b5ef5bf5fb17f6ca962
PatricioTristante <6808469@alu.murciaeduca.es>
2024-02-09 13:40:51 +0100

    Nuevos cambios

--- src/componentes/tarjetaProyecto/tarjetaProyecto.jsx
+++ src/componentes/tarjetaProyecto/tarjetaProyecto.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import './tarjetaProyecto.css';
+import imagenProyecto from '../../../public/proyecto.jpg'
 
 function TarjetaProyecto(props) {
 
+    function sacarAlumnos(alumno) {
+        return <span key={alumno.id}>{alumno.nombre} {alumno.apellidos} </span>;
+    }
+
+    function sacarCodCiclos(ciclo) {
+        return <span key={ciclo.id}>{ciclo.codCiclo} </span>;
+    }
+
     return (
-        <div className='col-12 col-md-4 col-lg-3'>
-            <div className="card fondoColorAzulado">
-                <img src="image.jpg" alt="Imagen de la tarjeta" className="card-img" />
-                <h2 className="card-title">Título de la tarjeta</h2>
-                <ul className="card-list fondoColorBlanquecino">
-                    <li>Elemento 1</li>
-                    <li>Elemento 2</li>
-                    <li>Elemento 3</li>
-                </ul>
+        <div className='col-12 col-md-4 col-lg-3 pb-3'>
+            <div className="card h-100 tarjeta">
+                <div className="card-body d-flex">
+                    <img src={ imagenProyecto } className="mr-3 imagenTarjeta" alt="Imagen de proyecto" />
+                    <div>
+                        <h5 className="card-title">{props.proyecto.nombre}</h5>
+                        <ul className="list-unstyled">
+                            <li><span className='negritaTarjeta'>Alumnos</span></li>
+                            {props.proyecto.estudiantes.map(sacarAlumnos)}
+                            <li><span className='negritaTarjeta'>Tutor:</span> {props.proyecto.docente_id}</li>
+                            <li><span className='negritaTarjeta'>Ciclos:</span></li>
+                            {props.proyecto.ciclos.map(sacarCodCiclos)}
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     );
